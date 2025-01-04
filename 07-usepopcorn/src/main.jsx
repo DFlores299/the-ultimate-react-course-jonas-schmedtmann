0063381cb4c66e46765257466e095fb1b41bccff
DFlores299 <dennisfloresescobar@gmail.com>
2025-01-03 20:04:53 -0500

    How NOT to Fetch Data in React

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,27 +1,22 @@
 import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-//import './index.css'
-//import App from './App.jsx'
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App.jsx";
+//import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movies was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <App />
   </StrictMode>
 );
