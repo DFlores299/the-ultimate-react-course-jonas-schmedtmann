99179bfcefa00a2d5c29d4bbf9b9def693c57734
DFlores299 <dennisfloresescobar@gmail.com>
2025-05-25 13:01:01 -0400

    Client-Side Sorting: Sorting Cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -13,6 +13,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -21,6 +22,19 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "name-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  // BUG this code from Jonas does not sort the name field properly (strings)
+  // const sortedCabins = filteredCabins.sort((a, b) => (a[field] - b[field]) * modifier);
+  // NOTE use this instead:
+  const sortedCabins = filteredCabins?.sort((a, b) =>
+    typeof a[field] === "string"
+      ? a[field].localeCompare(b[field]) * modifier
+      : (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -35,7 +49,8 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
